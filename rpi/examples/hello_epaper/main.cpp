a015ae1361d5f50781d8608969d6d216ec97b0b0
Larry Bank <bitbank@pobox.com>
2024-09-23 13:48:52 +0100

    changed to inkyphat 2.13

--- rpi/examples/hello_epaper/main.cpp
+++ rpi/examples/hello_epaper/main.cpp
@@ -5,16 +5,17 @@
 // Copyright (c) 2024 BitBank Software, Inc.
 //
 #include "../../../src/bb_epaper.h"
-BBEPAPER bbep(EPD27_176x264); // Waveshare 2.7" e-paper HAT
+BBEPAPER bbep(EPD213_104x212); // InkyPHAT 2.13"
 // BCM GPIO numbers
-#define PIN_DC 25
-#define PIN_RST 17
-#define PIN_BUSY 24
+#define PIN_DC 22
+#define PIN_RST 27
+#define PIN_BUSY 17
 #define PIN_CS 8
+#define SPI_BUS 0
 
 int main(int argc, const char * argv[]) {
-  bbep.initIO(PIN_DC, PIN_RST, PIN_BUSY, PIN_CS, 0, 8000000);
-  bbep.fillRect(0,0,128,296, BBEP_WHITE);
+  bbep.initIO(PIN_DC, PIN_RST, PIN_BUSY, PIN_CS, SPI_BUS, 8000000);
+  bbep.fillRect(0,0,104,212, BBEP_WHITE);
   bbep.setRotation(90);
   bbep.setFont(FONT_12x16);
   bbep.setTextColor(BBEP_BLACK, BBEP_WHITE);
